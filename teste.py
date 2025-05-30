55e1ced60fb501c32d277dd265cdab5bb84d8ad0
Kazuya <shinzatokazuya11@gmail.com>
2025-05-30 20:53:02 +0000

    Fri, May 30, 2025, 5:53 PM -03:00

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ import sqlite3
 import re
 
 # 1. Definir a URL do site que você quer analisar
-url = 'https://www.ogol.com.br/edicao/campeonato-brasileiro-2009/5268/calendario?equipa=0&estado=1&filtro=&op=calendario&page=8'
+url = 'https://www.ogol.com.br/edicao/campeonato-brasileiro-2010/13881/calendario?equipa=0&estado=1&filtro=&op=calendario&page=6'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 Edg/124.0.2478.80'
 }
