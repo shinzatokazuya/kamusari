df4f95b1d1a14063d454ebc036b180a0755ec476
Kazuya <shinzatokazuya11@gmail.com>
2025-11-13 02:32:56 +0000

    Wed, Nov 12, 2025, 11:32 PM -03:00

--- novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v2.py
+++ novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v2.py
@@ -631,6 +631,10 @@ class OGolScraperRelacional:
                         classe = " ".join(span.get("class", [])).lower()
                         texto_icone = span.get_text(strip=True)
 
+                        tempo_div = span.find_next_sibling("div")
+                        tempos_raw = tempo_div.get_text(" ", strip=True) if tempo_div else ""
+                        tempos = re.findall(r"\d+'\+?\d*'?\s*\(?[a-z.]*\)?", tempos_raw)
+
                         # Detecta o tipo de evento
                         if "gol" in title or "fut-11" in classe:
                             tipo_evento = "Gol"
