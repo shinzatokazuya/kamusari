5e23840af97c200faaddb5558e3a2692c0ded366
Kazuya <shinzatokazuya11@gmail.com>
2025-12-04 02:05:36 +0000

    Wed, Dec 3, 2025, 11:05 PM -03:00

--- novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
+++ novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
@@ -1260,6 +1260,6 @@ class OGolScraperRelacional:
 
 
 if __name__ == "__main__":
-    url = "https://www.ogol.com.br/edicao/campeonato-nacional-de-clubes-1973/2482/calendario?fase_in=0&equipa=0&estado=1&filtro=&page=1&op=calendario"
+    url = "https://www.ogol.com.br/edicao/campeonato-nacional-de-clubes-1973/2482/calendario?equipa=0&estado=1&filtro=&op=calendario&page=2"
     scraper = OGolScraperRelacional(url)
     scraper.executar(edicao_id=3)
