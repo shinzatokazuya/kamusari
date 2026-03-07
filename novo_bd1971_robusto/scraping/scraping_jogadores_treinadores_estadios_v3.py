5c161f48377264afeb5aba94abb0936356201084
Kazuya <shinzatokazuya11@gmail.com>
2026-03-07 18:07:16 +0000

    Sat, Mar 7, 2026, 3:07 PM -03:00

--- novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
+++ novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
@@ -440,7 +440,7 @@ class OGolScraperRelacional:
             return None
 
         dados = {}
-        for div in container.find_all("div", class_="card-data__body"):
+        for div in container.find_all("div", class_=["card-data__label", "card-data__value"]):
             span = div.find("span")
             if not span:
                 continue
