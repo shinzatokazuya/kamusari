f418d3b6f39a15b77280c102c729b008b86c49f8
Kazuya <shinzatokazuya11@gmail.com>
2026-03-25 01:58:04 +0000

    Tue, Mar 24, 2026, 10:58 PM -03:00

--- novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
+++ novo_bd1971_robusto/scraping/scraping_jogadores_treinadores_estadios_v3.py
@@ -1500,10 +1500,10 @@ if __name__ == "__main__":
     # ========== CONFIGURAÇÕES ==========
     min_page = 1
     max_page = 8
-    edicao_id = 40
+    edicao_id = 41
 
     # URL base (sem o parâmetro page)
-    url_base = "https://www.ogol.com.br/edicao/campeonato-brasileiro-2010/13881/calendario?fase_in=28651&equipa=0&estado=1&filtro=&op=calendario&page="
+    url_base = "https://www.ogol.com.br/edicao/campeonato-brasileiro-2011/21248/calendario?fase_in=37527&equipa=0&estado=1&filtro=&op=calendario&page="
 
     # Cria uma única instância que será reutilizada entre páginas
     # Isso mantém o CACHE, tornando muito mais rápido!
